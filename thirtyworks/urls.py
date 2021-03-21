9b98928a64d9f4297e017de0251970ce9d2f581e
Abdul <aabdulmajeed.isa@gmail.com>
2021-03-21 15:42:33 +0100

    setting changes

--- thirtyworks/urls.py
+++ thirtyworks/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete')
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
