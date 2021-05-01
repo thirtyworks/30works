1fc00d42c722c5f488a90b6f2110c088d9a1b594
Abdul <aabdulmajeed.isa@gmail.com>
2021-05-01 23:59:08 +0100

    add first and last name fields to export csv

--- users/admin.py
+++ users/admin.py
@@ -9,13 +9,19 @@ from django.http import HttpResponse
 # admin.site.register(UserProfile)
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_filter = ['blocked',         ('date_blocked', DropdownFilter),]
+    list_filter = ['blocked', ('date_blocked', DropdownFilter),]
 
-    list_display = ['user', 'get_email']
+    list_display = ['first_name', 'last_name','user', 'get_email']
 
     actions = ['export_as_csv']
 
-    search_fields = ['user__username', 'user__email']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'user__email']
+
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    def last_name(self, obj):
+        return obj.user.last_name
 
     def get_email(self, obj):
         return obj.user.email
@@ -28,14 +34,23 @@ class UserProfileAdmin(admin.ModelAdmin):
         response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
         writer = csv.writer(response)
 
-        writer.writerow(field_names+['email'])
+        # writer.writerow(field_names+['email'])
+        # for obj in queryset:
+        #     userprofile = obj
+        #     user = userprofile.user
+        #     email_address = user.email
+        #     print('email = {}'.format(email_address))
+        #     row = writer.writerow([getattr(obj, field) for field in field_names] + [email_address, first_name])
+        
+        # limited fields
+        writer.writerow(['first_name', 'last_name','email'])
         for obj in queryset:
             userprofile = obj
             user = userprofile.user
             email_address = user.email
-            print('email = {}'.format(email_address))
-            row = writer.writerow([getattr(obj, field) for field in field_names] + [email_address])
-
+            first_name = user.first_name
+            last_name = user.last_name
+            row = writer.writerow([ first_name, last_name, email_address])
         return response
 
     export_as_csv.short_description = "Export Selected"
