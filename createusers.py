d14c06d60cfdb2be319446b7c3a8ae80c41111f0
Abdul <aabdulmajeed.isa@gmail.com>
2022-03-25 10:08:10 +0100

    clean-up createusers

--- createusers.py
+++ createusers.py
@@ -5,18 +5,14 @@ or     $ python manage.py shell then run exec(open('createusers.py').read()) in 
 """
 from django.contrib.auth.models import User
 import pandas as pd
-import os
 from django.db.utils import IntegrityError
 from django.template.loader import render_to_string
 from django.core.mail import send_mail, send_mass_mail
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
-from django.utils import timezone
-from datetime import date, datetime
 from blog.views import get_brief, get_event_day
-import json
 
-CSV_FILE = 'test.csv'
+CSV_FILE = 'data.csv'
 
 def password_generator(size):
     import random
@@ -37,36 +33,22 @@ EMAIL_BRIEF_SUBJECT = f"30works30days {a_day} Brief"
 
 # read the CSV file
 df = pd.read_csv(CSV_FILE)
-# datatuple = (
-#     (
-#         EMAIL_SUBJECT, 
-#         EMAIL_MESSAGE.format(df['First Name'][i], create_username(df['First Name'][i], df['Surname'][i]), password_generator(7)), 
-#         FROM_EMAIL, 
-#         [df['Email'][i]]
-#     ) 
-#     for i in df.index
-# )
-# send_mass_mail(datatuple)
 
 for i in df.index:
-
-
     try:
         a_day = get_event_day()
         brief = get_brief()
-        first_name = df['First Name'][i]
-        first_name.strip()
-        surname = df['Surname'][i]
-        surname.strip()
-        email = df['Email'][i]
-        username = create_username(first_name, surname)
+        first_name = df['first_name'][i].strip()
+        last_name = df['last_name'][i].strip()
+        email = df['email'][i].strip()
+        username = create_username(first_name, last_name)
         password = password_generator(7)
         user = User.objects.create_user(
             username=username, 
             password=password, 
             email=email,
             first_name=first_name,
-            last_name=surname
+            last_name=last_name
         )
         user.is_superuser = False
         user.is_staff = False
@@ -110,30 +92,6 @@ for i in df.index:
 
     except IntegrityError as ie:
         print(ie)
-        print(f'Unable to create User: {username}\nEmail: {email}\nPass: {password}\n==========')
+        print(f'Unable to create\nUser: {username}\nEmail: {email}\nPass: {password}\n==========')
 
 print('done!')
-
-
-
-
-
-# for i, row in df.iterrows():
-#     username = row['First Name'] + '_' + row['Surname']
-#     email_address = row['Email Address']
-#     password = id_generator(10)
-#     try:
-#         user = User.objects.create_user(username=username, password=password, email=email_address)
-#         user.is_superuser = False
-#         user.is_staff = False
-#         user.save()
-
-#         print('Created User: {}\nEmail: {}\nPass: {}\n=========='.format(username, email_address, password))
-
-#         email_message = "{}".format(EMAIL_MESSAGE)
-#         email_message = email_message.format(username, password)
-#         email(EMAIL_SUBJECT, email_message, [email_address])
-
-#     except IntegrityError as ie:
-#         print(ie)
-#         print('Couldnt create User: {}\nEmail: {}\nPass: {}\n=========='.format(username, email_address, password))
