0bdd1501fb756152c5ff757f1d504a8dbafa60f5
Abdul <aabdulmajeed.isa@gmail.com>
2022-04-07 22:21:09 +0100

    add force log out session function

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,4 +1,7 @@
+from urllib import request
 from .models import Day
+from django.contrib.auth import logout
+import datetime
 
 def GetDate(request):
     days = Day.objects.last()
@@ -11,3 +14,20 @@ def get_latest_day_number(request):
     else:
         day_number = 1
     return {"day_number": 1}
+
+
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Check if user session is still logged in after user is blocked then force log them out.
+        if request.user.is_authenticated and not request.user.is_active:
+            logout(request)
+        return response
--- thirtyworks/settings/base.py
+++ thirtyworks/settings/base.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blog.context_processors.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'thirtyworks.urls'
