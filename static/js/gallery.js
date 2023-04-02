5af7c948281f8b28fe279737dbad67025e4cbd68
Abdul <aabdulmajeed.isa@gmail.com>
2023-04-02 23:39:34 +0100

    fix gallery

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,6 +1,7 @@
 (function () {
   $(document).ready(function () {
-    $(".gallery a").magnificPopup({
+    $(".gallery").magnificPopup({
+      delegate: 'a',
       gallery: {
         enabled: false,
       },
@@ -36,4 +37,6 @@
       },
     });
   });
+
+
 })();
